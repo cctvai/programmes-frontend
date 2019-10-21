95f6b686267c4e4fae8849657405803599d88823
Sam Fry <benolot@gmail.com>
2019-10-21 09:34:37 +0100

    [PROGRAMMES-6149] Add logging for all failed HTTP calls (#1200)

--- src/Metrics/MetricsMiddleware.php
+++ src/Metrics/MetricsMiddleware.php
@@ -3,7 +3,6 @@
 namespace App\Metrics;
 
 use App\ExternalApi\ApiType\Mapper\UriToApiTypeMapper;
-use App\Fixture\ScenarioManager;
 use GuzzleHttp\TransferStats;
 use Psr\Http\Message\RequestInterface;
 use Psr\Log\LoggerInterface;
@@ -29,20 +28,26 @@ class MetricsMiddleware
     public function __invoke(callable $handler)
     {
         return function (RequestInterface $request, array $options) use ($handler) {
-            $this->logger->info("HTTP Request:" . $request->getUri());
+            $this->logger->info("HTTP Request: " . $request->getUri());
 
             $options['on_stats'] = function (TransferStats $stats) use (&$responseTime) {
                 $uri = $stats->getEffectiveUri();
                 $apiName = $this->uriToApiTypeMapper->getApiNameFromUriInterface($uri);
-                if (!$apiName) {
-                    return;
-                }
                 $responseTime = (int) round($stats->getTransferTime() * 1000);
                 // No response/timeout is logged as a 504 (gateway timeout). Which isn't correct. But whatever.
                 $responseCode = 504;
                 if ($stats->hasResponse()) {
                     $responseCode = $stats->getResponse()->getStatusCode();
                 }
+
+                if ($responseCode >= 400 && $responseCode <= 599 && $responseCode !== 404) {
+                    $this->logger->error('HTTP request failed: ' . $uri . ' - got status code ' . $responseCode);
+                }
+
+                if (!$apiName) {
+                    return;
+                }
+
                 $this->metricsManager->addApiMetric($apiName, $responseTime, $responseCode);
             };
 
